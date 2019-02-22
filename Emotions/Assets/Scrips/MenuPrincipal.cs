46c971ba4f529751d9f3edb25dbaeb110fdd9386
hernyyc <43017231+hernyyc@users.noreply.github.com>
2019-02-22 19:06:51 -0300

    Commit del 22-02-19
    Cambios finales al TIF. Se añadieron Escenas de Ayuda

--- Emotions/Assets/Scrips/MenuPrincipal.cs
+++ Emotions/Assets/Scrips/MenuPrincipal.cs
@@ -19,7 +19,7 @@ public class MenuPrincipal : MonoBehaviour
 
     public void Exp_Grad()
     {
-        SceneManager.LoadScene("Escena_EG2");
+        SceneManager.LoadScene("Escena_EG");
     }
     public void Salir()
     {
