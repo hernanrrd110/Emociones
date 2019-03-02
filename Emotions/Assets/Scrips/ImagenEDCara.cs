32535dc3f9004920dec968dce88559d3aca8b6c6
hernyyc <43017231+hernyyc@users.noreply.github.com>
2019-03-02 13:51:15 -0300

    Commit 02-02-19
    Cambios mínimos visuales a algunas escenas

--- Emotions/Assets/Scrips/ImagenEDCara.cs
+++ Emotions/Assets/Scrips/ImagenEDCara.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class ImagenEDCara : MonoBehaviour
 {
 
     public Image imagenCara;
-
+    public Slider slider;
     public Imagen_TIF[] carucha;
-
+    public TextMeshProUGUI texto;
 
     public void CambioSlider(float valor)
     {
         imagenCara.sprite = carucha[(int) valor].imagen;
+        texto.text = valor.ToString();
 
     }
 
-
+   
 }
